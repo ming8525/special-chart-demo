4be7814d3bcd29c062c3405d50044c1223221084
Mingyang Han <ming8525@esri.com>
2023-04-19 16:50:25 +0800

    fix: make demo, save progress

--- src/index.jsx
+++ src/index.jsx
@@ -10,47 +10,80 @@ applyPolyfills().then(() => {
 })
 
 const CacheLayers = {}
-const createFeatureLayer = (portalUrl, itemId) => {
-  return new Promise((resolve, reject) => {
-    if (!portalUrl || !itemId) return reject()
-    if (!CacheLayers[itemId]) {
-      const fl = new FeatureLayer({
-        portalItem: {
-          id: itemId,
-          portal: { url: portalUrl }
-        }
-      })
-      fl.load().then((layer) => {
-        CacheLayers[itemId] = layer
-        resolve(layer)
-      })
-    } else {
-      resolve(CacheLayers[itemId])
-    }
-  })
+const createFeatureLayer = (url) => {
+  if (!url) return null
+  if (!CacheLayers[url]) {
+    const fl = new FeatureLayer({ url })
+    CacheLayers[url] = fl
+    return fl
+  } else {
+    return CacheLayers[url]
+  }
 }
-const portalUrl = 'https://www.arcgis.com/'
-const itemId = 'a3880aef9fee444c84da17c0d3b92630'
+
+const useRegisterEvent = (nodeRef, eventName, callback) => {
+  React.useEffect(() => {
+    const node = nodeRef?.current
+    if (node && eventName && callback) {
+      node.addEventListener(eventName, callback)
+    }
+
+    return () => {
+      if (node && eventName && callback) {
+        node.removeEventListener(eventName, callback)
+      }
+    }
+  }, [eventName, nodeRef, callback])
+}
+
+const ChartLimits = {"behaviorAfterLimit":"reject","maxBarUniqueSeriesCountTotal":10000,"maxBarTwoSeriesCountPerSeries":1000,"maxBarTwoSeriesCountTotal":2000,"maxBarThreePlusSeriesCountPerSeries":100,"maxBarThreePlusSeriesCountTotal":2000,"maxScatterPointsBeforeAggregation":10000,"maxScatterPointsAfterAggregation":10000,"maxLineChartMarkersCountTotal":10000,"maxLineChartSeriesCount":100,"maxPieChartSliceCountTotal":300}
+
+const ServiceURL = 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3'
 
 const Root = (props) => {
   const ref = React.useRef(null)
   const [featureLayer, setFeatureLayer] = React.useState(null)
 
   React.useEffect(() => {
-    createFeatureLayer(portalUrl, itemId).then((featureLayer) => {
-      setFeatureLayer(featureLayer)
-    })
+    const featureLayer = createFeatureLayer(ServiceURL)
+    setFeatureLayer(featureLayer)
   }, [])
+
+  const arcgisChartsDataProcessComplete = React.useCallback((event) => {
+    console.log('arcgisChartsDataProcessComplete', event.detail)
+  }, [])
+
+  const arcgisChartsDataProcessError = React.useCallback((event) => {
+    console.log('arcgisChartsDataProcessError')
+  }, [])
+
+  const arcgisChartsSelectionComplete = React.useCallback((event) => {
+    console.log('arcgisChartsSelectionComplete', event.detail)
+  }, [])
+
 
   React.useEffect(() => {
     if(ref.current && featureLayer) {
       ref.current.config = config
-      ref.current.featureLayer = featureLayer
+      ref.current.layer = featureLayer
+      ref.current.returnSelectionIndexes = true
+      ref.current.returnSelectionOIDs = false
+      ref.current.queueChartCreation = true
+      ref.current.autoDisposeChart = true
+      ref.current.enableResponsiveFeatures = false
+      ref.current.useAnimatedCharts = false
+      ref.current.chartLimits = ChartLimits
+
+      ref.current.selectionData = { selectionIndexes: undefined }
     }
   }, [featureLayer])
 
+  useRegisterEvent(ref, 'arcgisChartsDataProcessComplete', arcgisChartsDataProcessComplete)
+  useRegisterEvent(ref, 'arcgisChartsDataProcessError', arcgisChartsDataProcessError)
+  useRegisterEvent(ref, 'arcgisChartsSelectionComplete', arcgisChartsSelectionComplete)
+
   return <div style={{ height: 400 }}>
-    {featureLayer && <arcgis-charts-bar-chart ref={ref} />}
+    <arcgis-charts-pie-chart ref={ref} />
   </div>
 }
 
