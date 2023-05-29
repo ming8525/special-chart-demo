3a6f943a3e6f1b34f2df1299286965521628bb92
Mingyang Han <ming8525@esri.com>
2023-05-29 14:06:16 +0800

    fix: male demo for legend disappear

--- src/index.jsx
+++ src/index.jsx
@@ -21,64 +21,24 @@ const createFeatureLayer = (url) => {
   }
 }
 
-const createDefaultSelectionData = () => {
-  const selectionIndexes = new Map()
-  selectionIndexes.set(0, { indexesToSelect: [-1] })
-  const selectionData = { selectionIndexes }
-  return selectionData
-}
-
-const getNextSelection = (inputSelectionData) => {
-  let newSelectionData = inputSelectionData
-  const selectionIndexes = newSelectionData.selectionIndexes
-  const selectionIndex = selectionIndexes.get(0).indexesToSelect[0] + 1
-  selectionIndexes.set(0, { indexesToSelect: [selectionIndex] })
-  return { selectionIndexes }
-}
-
-const BarChart = ({ title = 'Chart 1', style }) => {
+const BarChart = ({ style }) => {
   const ref = React.useRef(null)
 
   React.useEffect(() => {
     const service = config.service
     const layer = createFeatureLayer(service)
 
-    let webChart = config.webChart
-
-    webChart = {
-      ...webChart,
-      title: {
-        ...webChart.title,
-        content: {
-          ...webChart.title.content,
-          text: title
-        }
-      },
-      rotated: Math.random() >= 0.5
-    }
+    const webChart = config.webChart
     ref.current.config = webChart
     ref.current.layer = layer
-
-    setTimeout(() => {
-      let newSelectionData = getNextSelection(createDefaultSelectionData())
-      ref.current.selectionData = newSelectionData
-      setTimeout(() => {
-        newSelectionData = getNextSelection(newSelectionData)
-        ref.current.selectionData = newSelectionData
-        setTimeout(() => {
-          newSelectionData = getNextSelection(newSelectionData)
-          ref.current.selectionData = newSelectionData
-        }, 200)
-      }, 200)
-    }, 200)
   }, [])
 
-  return <div style={{ height: '100%', width: '100%', transform: 'translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg)', ...style }}>
+  return <div style={{ height: '100%', width: '100%', ...style }}>
     <arcgis-charts-bar-chart ref={ref} />
   </div>
 }
 
-const Charts = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['10', '11', '12']]
+const Charts = ['1', '2']
 
 const Root = (props) => {
   const [displayIndex, setDisplayIndex] = React.useState(0)
@@ -92,17 +52,11 @@ const Root = (props) => {
   }
 
   return <div style={{ height: 300 }}>
-    <div style={{ height: '100%', width: '1200px', position: 'relative' }}>
+    <div style={{ height: '100%', width: '1200px' }}>
       {
-        Charts.map((subCharts, idx) => {
+        Charts.map((chart, idx) => {
           const activate = displayIndex === idx
-          return <div key={idx} style={{ height: '100%', width: '1200px', position: 'absolute', display: activate ? 'flex' : 'none', opacity: activate ? 1 : 0, zIndex: activate ? 1 : 0 }}>
-              {
-                subCharts.map((titleIndex, subIdx) => {
-                  return <BarChart key={`${idx}-${subIdx}`} title={`Chart ${titleIndex}`} />
-                })
-              }
-          </div>
+          return <BarChart key={idx} style={{ display: activate ? 'flex' : 'none' }}/>
         })
       }
     </div>
