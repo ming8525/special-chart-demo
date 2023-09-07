1071f00440646c9b6110d330d767e1a8031a9f41
Mingyang Han <ming8525@esri.com>
2023-09-07 17:07:38 +0800

    fix: progressing

--- src/index.jsx
+++ src/index.jsx
@@ -17,33 +17,89 @@ const createFeatureLayer = (url) => {
 const convertTimeExtent = (timeExtent) => {
   return {
     start: new Date(timeExtent[0]),
-    end: new Date(timeExtent[1])
+    end: new Date(timeExtent[1]),
   }
 }
 
-const timeExtent = convertTimeExtent([1625029260000, 1627621260000])
+const runtimeDataFilters1 = {
+  geometry: {
+    spatialReference: {
+      latestWkid: 3857,
+      wkid: 102100
+    },
+    xmin: -10305767.857059883,
+    ymin: 3453350.867722927,
+    xmax: -10196768.654725181,
+    ymax: 3606530.6724064793
+  }
+}
+
+const runtimeDataFilters2 = {
+  geometry: {
+    spatialReference: {
+      latestWkid: 3857,
+      wkid: 102100
+    },
+    xmin: -10305767.857059883,
+    ymin: 3453350.867722927,
+    xmax: -10196768.654725181,
+    ymax: 3606530.6724064793
+  }
+}
+
+const runtimeDataFilters3 = {
+  geometry: {
+    spatialReference: {
+      latestWkid: 3857,
+      wkid: 102100
+    },
+    xmin: -10305767.857059883,
+    ymin: 3453350.867722927,
+    xmax: -10196768.654725181,
+    ymax: 3606530.6724064793
+  }
+}
+
+const timeExtent1 = convertTimeExtent([1625029260000, 1627621260000])
+const timeExtent2 = convertTimeExtent([1627621260000, 1630299660000])
 
 const Root = (props) => {
   const ref = React.useRef()
+  const layerRef = React.useRef(createFeatureLayer(config.service))
 
   React.useEffect(() => {
-    const layer = createFeatureLayer(config.service)
+    const layer = layerRef.current
     layer.load().then(() => {
       const webChart = config.webChart
       ref.current.config = webChart
       ref.current.layer = layer
+      ref.current.runtimeDataFilters = runtimeDataFilters1
       setTimeout(() => {
-        layer.timeExtent = timeExtent
+        layer.timeExtent = timeExtent1
         ref.current.layer = layer
-        ref.current.refresh()
-      }, 500);
+        setTimeout(() => {
+          ref.current.refresh()
+        }, 11)
+      }, 2826)
     })
   }, [])
 
+  const handleClick = () => {
+    ref.current.runtimeDataFilters = runtimeDataFilters2
+    setTimeout(() => {
+      const layer = layerRef.current
+      layer.timeExtent = timeExtent2
+      ref.current.layer = layer
+      setTimeout(() => {
+        ref.current.refresh()
+      }, 11)
+    }, 1)
+  }
 
   return (
-    <div style={{ height: 500, width: '100%', display: 'flex' }} className='border'>
+    <div style={{ height: 323, width: 738 }} className='border'>
       <arcgis-charts-line-chart ref={ref} />
+      <button onClick={handleClick}>Next time</button>
     </div>
   )
 }
