61e86dfb7c296f6940490458d63563ac8146fa96
Mingyang Han <ming8525@esri.com>
2023-09-13 10:56:46 +0800

    fix: enable drag and drop

--- src/index.jsx
+++ src/index.jsx
@@ -24,26 +24,54 @@ const Root = (props) => {
   const container2 = React.useRef()
 
   React.useEffect(() => {
-      const layer = createFeatureLayer(config.service)
-      const webChart = config.webChart
-      ref.current.config = webChart
-      ref.current.layer = layer
+    const layer = createFeatureLayer(config.service)
+    const webChart = config.webChart
+    ref.current.config = webChart
+    ref.current.layer = layer
   }, [])
 
   const handleSwitchContainer = () => {
-    if(container1.current.hasChildNodes()) {
+    if (container1.current.hasChildNodes()) {
       container2.current.appendChild(ref.current)
     } else {
       container1.current.appendChild(ref.current)
     }
   }
 
+  const draggedRef = React.useRef(null)
+
+  const handleDrag = (event) => {
+    event.target.classList.add("dragging");
+    draggedRef.current = event.target
+  }
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  }
+
+  const handleDragEnd = () => {
+    event.target.classList.remove("dragging");
+    event.target.refresh()
+  }
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    if (event.target.className.includes("dropzone")) {
+      event.target.appendChild(draggedRef.current);
+    }
+  }
+
   return <div>
     <div style={{ height: 500, width: '100%', display: 'flex' }}>
-      <div className='border' style={{ height: '100%', width: '100%' }} ref={container1}>
-        <arcgis-charts-bar-chart ref={ref} />
+      <div id="container" className='border' style={{ height: '100%', width: '100%' }} ref={container1}>
+        <arcgis-charts-bar-chart
+          ref={ref}
+          draggable="true"
+          onDragStart={handleDrag}
+          onDragEnd={handleDragEnd}
+        />
       </div>
-      <div className='border' style={{ height: '100%', width: '100%' }} ref={container2}></div>
+      <div className="dropzone border" onDragOver={handleDragOver} onDrop={handleDrop} style={{ height: '100%', width: '100%' }} ref={container2}></div>
     </div>
     <button onClick={handleSwitchContainer}>Switch container</button>
   </div>
