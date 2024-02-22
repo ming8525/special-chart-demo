1d78b0363b530e948ea8d6b9fc54110817d190bd
Mingyang Han <Mingyang_Han@esri.com>
2024-02-22 10:06:38 +0800

    fix: make demo for v4.29.0-beta.32

--- src/index.jsx
+++ src/index.jsx
@@ -11,33 +11,22 @@ applyPolyfills().then(() => {
   defineCustomElements(window, { resourcesUrl: '../arcgis-charts/' })
 })
 
-const selectionData = {
-  selectionItems: [
-    {
-      count_of_FID: 1,
-      FID: 2
-    }
-  ]
-}
-
 const Root = (props) => {
   const chartRef = React.useRef()
 
   React.useEffect(() => {
     chartRef.current.config = config
-    chartRef.current.selectionData = selectionData
   }, [])
 
 
   return (
     <div className='d-flex'>
       <div
-        style={{ height: 600, width: 600 }}
+        style={{ height: 500, width: 700 }}
         className='border'
       >
-        <arcgis-charts-pie-chart ref={chartRef} />
+        <arcgis-charts-bar-chart ref={chartRef} />
       </div>
-      <pre className='border' style={{ width: 360, wordBreak: 'break-word' }}>{JSON.stringify(selectionData, null, " ")}</pre>
     </div>
   )
 }
