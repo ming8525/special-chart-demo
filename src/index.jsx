ebc8782379dae1482b2eef54999ea1c98c3d5b98
Mingyang Han <Mingyang_Han@esri.com>
2024-05-11 09:44:22 +0800

    fix: update demo

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,6 @@ import config from './config.json'
 import './style.css'
 import WebMap from '@arcgis/core/WebMap'
 import MapView from '@arcgis/core/views/MapView'
-import esriConfig from '@arcgis/core/config'
 import '@arcgis/core/assets/esri/themes/light/main.css'
 
 applyPolyfills().then(() => {
@@ -18,39 +17,39 @@ applyPolyfills().then(() => {
 const Root = (props) => {
   const chartRef = React.useRef()
   const mapDivRef = React.useRef()
-  const [refreshOnViewExtentChange, setRefreshOnViewExtentChange] = React.useState(true)
-
-  const handleViewChange = React.useCallback(async (view) => {
-    await view.when()
-
-    const layer = view.map.layers.toArray()[0]
-
-    chartRef.current.config = config.webChart
-    chartRef.current.layer = layer
-    chartRef.current.view = view
-
-    chartRef.current.refreshOnViewExtentChange = refreshOnViewExtentChange
-  }, [chartRef])
+  const [viewExtentChangePolicy, setViewExtentChangePolicy] = React.useState('refresh')
 
   React.useEffect(() => {
-    const webmap = new WebMap({
-      portalItem: {
-        id: 'f41763f3ec144ac4b771c7b8d17cca11',
-      },
-    })
-    const view = new MapView({
-      container: mapDivRef.current,
-      map: webmap,
-      zoom: 10,
-      center: [-87.85, 41.80]
-    })
-    handleViewChange(view)
+    (async () => {
+      const webmap = new WebMap({
+        portalItem: {
+          id: 'f41763f3ec144ac4b771c7b8d17cca11',
+        },
+      })
+      const view = new MapView({
+        container: mapDivRef.current,
+        map: webmap,
+        zoom: 10,
+        center: [-87.85, 41.80]
+      })
+
+      await view.when()
+
+      const layer = view.map.layers.toArray()[0]
+
+      chartRef.current.config = config.webChart
+      chartRef.current.layer = layer
+      chartRef.current.view = view
+
+      chartRef.current.viewExtentChangePolicy = 'refresh'
+    })()
   }, [])
 
   const handleRefreshOnViewExtentChange = (e) => {
     const checked = e.target.checked
-    chartRef.current.refreshOnViewExtentChange = checked
-    setRefreshOnViewExtentChange(checked)
+    const changePolicy = checked ? 'refresh' : 'ignore'
+    chartRef.current.viewExtentChangePolicy = changePolicy
+    setViewExtentChangePolicy(changePolicy)
   }
 
   return (
@@ -67,8 +66,8 @@ const Root = (props) => {
         <arcgis-charts-bar-chart ref={chartRef} />
       </div>
       <div>
-        <input type="checkbox" id="refreshOnViewExtentChange" checked={refreshOnViewExtentChange} onChange={handleRefreshOnViewExtentChange} />
-        <label htmlFor="refreshOnViewExtentChange">RefreshOnViewExtentChange</label>
+        <input type="checkbox" id="viewExtentChangePolicy" checked={viewExtentChangePolicy === 'refresh'} onChange={handleRefreshOnViewExtentChange} />
+        <label htmlFor="viewExtentChangePolicy">{viewExtentChangePolicy}</label>
       </div>
     </div>
   )
