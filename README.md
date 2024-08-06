32756cad6812e59ce8f5b239caac0fd8bc6b7e79
Mingyang Han <Mingyang_Han@esri.com>
2024-08-06 13:46:15 +0800

    feat: make demo for sub type group layer

--- README.md
+++ README.md
@@ -4,13 +4,8 @@ This repository is used to provide some demos about arcgis charts problems that 
 
 # How to start
 
-- Clone this repo
+- Clone this repo and switch to branch 'f-sub-group-sub-layer'
 - Run `npm ci`
 - Run `npm start`
 - Open http://localhost:8080/
-- Register a ClientID
-  - Since the demo data is on Ess Org, we need to register a ClientID first
-  - Log in to ess org and [register a ClientID](https://developers.arcgis.com/documentation/security-and-authentication/api-key-authentication/tutorials/create-an-api-key/#steps)
-  - Make sure to set the Redirect URL to `http://localhost:8080`
-- Paste the ClientID into the appropriate input box and then click 'Sign In'
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,82 +1,32 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
 import { defineCustomElements } from '@arcgis/charts-components/dist/loader'
-import { useRegisterOAuth } from './inentity-manager'
+import SuptypeGroupLayer from '@arcgis/core/layers/SubtypeGroupLayer'
 import config from './config.json'
 import './style.css'
 defineCustomElements(window, { resourcesUrl: '../arcgis-charts/' })
 
-const Chart = () => {
-  const chartRef = React.useRef()
-  React.useEffect(() => {
-    chartRef.current.config = config
-  }, [])
-  return <arcgis-charts-bar-chart ref={chartRef} />
+const SubGroupLayerURL = 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/MarineLife/FeatureServer/0'
+
+const createSubGroupSubLayer = (url) => {
+  const layer = new SuptypeGroupLayer({ url })
+  return layer.loadAll().then(() => {
+    return layer.sublayers.find((layer) => layer.title === 'Echinoderm')
+  })
 }
 
 const Root = (props) => {
-  const clientIdRef = React.useRef()
-  const portalUrlRef = React.useRef()
-  const [oAuthState, handleSignIn] = useRegisterOAuth()
-  const [activated, setActivated] = React.useState('second')
+  const chartRef = React.useRef()
 
-  const handleClickSignIn = () => {
-    const clientId = clientIdRef.current.value
-    const portalUrl = portalUrlRef.current.value
-    handleSignIn(clientId, portalUrl)
-  }
+  React.useEffect(() => {
+    createSubGroupSubLayer(SubGroupLayerURL).then((layer) => {
+      chartRef.current.layer = layer
+      chartRef.current.config = config
+    })
+  }, [])
 
   return (
-    <div>
-      {oAuthState === 'not_ready' && <div>
-        <label>
-          Portal URL:
-          <input style={{ width: 200 }} ref={portalUrlRef} type='text' value='https://essorg.maps.arcgis.com' disabled />
-        </label>
-        <label>
-          <a href='https://developers.arcgis.com/documentation/security-and-authentication/api-key-authentication/tutorials/create-an-api-key/' target='_blank'>ClientId:</a>
-          <input ref={clientIdRef} type='password' />
-        </label>
-        <button onClick={handleClickSignIn}>Sign in</button>
-      </div>}
-      {oAuthState === 'ready' && (
-        <div className='container border'>
-          <div
-            className='header border-bottom'
-            style={{ width: '100%', display: 'flex' }}
-          >
-            <button
-              className={activated === 'first' ? 'activated' : ''}
-              style={{ width: '50%' }}
-              onClick={() => setActivated('first')}
-            >
-              First
-            </button>
-            <button
-              className={activated === 'second' ? 'activated' : ''}
-              style={{ width: '50%' }}
-              onClick={() => setActivated('second')}
-            >
-              Second
-            </button>
-          </div>
-          <div className='contents'>
-            <div
-              className='content first-content'
-              style={{ display: activated === 'first' ? 'block' : 'none' }}
-            >
-              Please switch to the second tab content
-            </div>
-            <div
-              className='content second-content'
-              style={{ display: activated === 'second' ? 'block' : 'none' }}
-            >
-              <Chart />
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    <arcgis-charts-bar-chart ref={chartRef} />
   )
 }
 
