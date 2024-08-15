2396810d03e40339e6f307a14a8cac95e99ae4e2
Mingyang Han <Mingyang_Han@esri.com>
2024-08-15 17:45:14 +0800

    fix: make demo for enterprise-11.4

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
-import {
-  applyPolyfills,
-  defineCustomElements,
-} from '@arcgis/charts-components/dist/loader'
-import config from './config.json'
+import { applyPolyfills, defineCustomElements } from '@arcgis/charts-components/dist/loader'
+import defaultConfig from './config.json'
+import lodash from 'lodash'
 import './style.css'
 
 applyPolyfills().then(() => {
@@ -13,11 +11,18 @@ applyPolyfills().then(() => {
 
 const Root = (props) => {
   const chartRef = React.useRef()
+  const [config, setConfig] = React.useState(defaultConfig)
 
   React.useEffect(() => {
     chartRef.current.config = config
-  }, [])
+  }, [config])
 
+  const handleClick = () => {
+    const isASC = config.series[0].query.orderByFields[0].includes('ASC')
+    const orderByFields = [`Year ${isASC ? 'DESC' : 'ASC'}`]
+    const newConfig = lodash.set(config, 'series[0].query.orderByFields', orderByFields)
+    setConfig(lodash.cloneDeep(newConfig))
+  }
 
   return (
     <div className='d-flex'>
@@ -27,6 +32,7 @@ const Root = (props) => {
       >
         <arcgis-charts-bar-chart ref={chartRef} />
       </div>
+      <button onClick={handleClick}>{config.series[0].query.orderByFields[0]}</button>
     </div>
   )
 }
