32ea1f8d65b356f8fab1bd6224162edb84632c5d
Mingyang Han <Mingyang_Han@esri.com>
2024-08-19 11:22:02 +0800

    feat: make demo for returnSelectionOIDs

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
-import { applyPolyfills, defineCustomElements } from '@arcgis/charts-components/dist/loader'
+import { defineCustomElements } from '@arcgis/charts-components/dist/loader'
 import SuptypeGroupLayer from '@arcgis/core/layers/SubtypeGroupLayer'
 import config from './config.json'
 import './style.css'
 
-applyPolyfills().then(() => {
-  defineCustomElements(window, { resourcesUrl: '../arcgis-charts/' })
-})
+defineCustomElements(window, { resourcesUrl: '../arcgis-charts/' })
 
-const SubGroupLayerURL = 'https://services1.arcgis.com/oC086ufSSQ6Avnw2/arcgis/rest/services/Subtype_group_layer_unique_value_using_material_and_width_field1_WFL1/FeatureServer/0'
+const SubGroupLayerURL = 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/MarineLife/FeatureServer/0'
 
 const createSubGroupSubLayer = (url) => {
   const layer = new SuptypeGroupLayer({ url })
-  return layer.loadAll(() => {
-    return layer
+  return layer.loadAll().then(() => {
+    return layer.sublayers.find((layer) => layer.title === 'Echinoderm')
   })
 }
 
@@ -25,17 +23,13 @@ const Root = (props) => {
     createSubGroupSubLayer(SubGroupLayerURL).then((layer) => {
       chartRef.current.layer = layer
       chartRef.current.config = config
+      chartRef.current.returnSelectionOIDs = true
     })
   }, [])
 
   return (
-    <div className='d-flex'>
-      <div
-        style={{ height: 600, width: 600 }}
-        className='border'
-      >
-        <arcgis-charts-histogram ref={chartRef} />
-      </div>
+    <div style={{ height: 600, width: 600 }} className='border'>
+      <arcgis-charts-bar-chart ref={chartRef} />
     </div>
   )
 }
