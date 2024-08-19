82b670a56f4546bc654cb99eed1d77045d0e3afc
Mingyang Han <Mingyang_Han@esri.com>
2024-08-19 10:41:56 +0800

    fix: processing

--- src/index.jsx
+++ src/index.jsx
@@ -2,67 +2,22 @@ import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
 import { ArcgisChartsBarChart } from '@arcgis/charts-components-react'
 import { defineCustomElements } from '@arcgis/charts-components/dist/loader'
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
+import SuptypeGroupLayer from '@arcgis/core/layers/SubtypeGroupLayer'
 import config from './config.json'
-import fakerLayer from './faker-layer.json'
 import './style.css'
 defineCustomElements(window, { resourcesUrl: '../arcgis-charts/' })
 
-const Chart = () => {
-  const [layer, setLayer] = React.useState(null)
-  
-  React.useEffect(() => {
-    const layer = new FeatureLayer({
-      objectIdField: "FID",
-      fields: fakerLayer.fields,
-      source: fakerLayer.features
-    })
-    setLayer(layer)
-  }, [])
+const SubGroupLayerURL = 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/MarineLife/FeatureServer/0'
 
-  return <ArcgisChartsBarChart layer={layer} config={config} />
+const createSubGroupSubLayer = (url) => {
+  const layer = new SuptypeGroupLayer({ url })
+  return layer.loadAll().then(() => {
+    return layer.sublayers.find((layer) => layer.title === 'Echinoderm')
+  })
 }
 
-const Root = (props) => {
-  const [activated, setActivated] = React.useState('second')
-
-  return (
-    <div className='container border'>
-      <div
-        className='header border-bottom'
-        style={{ width: '100%', display: 'flex' }}
-      >
-        <button
-          className={activated === 'first' ? 'activated' : ''}
-          style={{ width: '50%' }}
-          onClick={() => setActivated('first')}
-        >
-          First
-        </button>
-        <button
-          className={activated === 'second' ? 'activated' : ''}
-          style={{ width: '50%' }}
-          onClick={() => setActivated('second')}
-        >
-          Second
-        </button>
-      </div>
-      <div className='contents'>
-        <div
-          className='content first-content'
-          style={{ display: activated === 'first' ? 'block' : 'none' }}
-        >
-          Please switch to the second tab content
-        </div>
-        <div
-          className='content second-content'
-          style={{ display: activated === 'second' ? 'block' : 'none' }}
-        >
-          <Chart />
-        </div>
-      </div>
-    </div>
-  )
+const Root = () => {
+  return <ArcgisChartsBarChart config={config} />
 }
 
 const app = ReactDOMClient.createRoot(document.getElementById('app'))
