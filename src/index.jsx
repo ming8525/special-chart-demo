6738da354ee98769548a62e8ca0d5fd90f1ee874
Mingyang Han <Mingyang_Han@esri.com>
2024-10-08 16:40:14 +0800

    feat: auto switch chart component type

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
-import { ArcgisChartsBarChart } from '@arcgis/charts-components-react'
+import { ArcgisChartsBarChart, ArcgisChartsLineChart, ArcgisChartsPieChart } from '@arcgis/charts-components-react'
 import { defineCustomElements } from '@arcgis/charts-components/dist/loader'
 import { JsonEditor } from './json-editor'
 import config from './config.json'
@@ -12,6 +12,7 @@ defineCustomElements(window, { resourcesUrl: '../arcgis-charts/' })
 const Root = (props) => {
   const editorRef = React.useRef(null)
   const [webChart, setWebChart] = React.useState()
+  const seriesType = webChart?.series[0]?.type ?? 'barSeries'
 
   const handleUpdate = () => {
     setWebChart(editorRef.current.get())
@@ -19,7 +20,9 @@ const Root = (props) => {
 
   return (
     <div style={{ height: 800, width: 1600, display: 'flex' }}>
-      <ArcgisChartsBarChart config={webChart} style={{ height: 800, width: 800 }} className='border' />
+      {seriesType === 'barSeries' && <ArcgisChartsBarChart config={webChart} style={{ height: 800, width: 800 }} className='border' />}
+      {seriesType === 'lineSeries' && <ArcgisChartsLineChart config={webChart} style={{ height: 800, width: 800 }} className='border' />}
+      {seriesType === 'lpieSeries' && <ArcgisChartsPieChart config={webChart} style={{ height: 800, width: 800 }} className='border' />}
       <div style={{ height: 800, width: 800 }} className='border'>
         <JsonEditor ref={editorRef} defaultValue={config} onUpdate={handleUpdate} />
       </div>
